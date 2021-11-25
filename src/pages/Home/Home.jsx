9fd7de0274b61e8fffe354615df0c53e04676fc5
MisterAzix <flizi@live.fr>
2021-11-25 17:43:50 +0100

    :sparkles: Router setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from '../../logo.svg';
+import styles from './home.module.css';
+
+const Home = () => {
+    return (
+        <div className={styles.App}>
+            <header className={styles.header}>
+                <img src={logo} className={styles.logo} alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className={styles.link}
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+};
+
+export default Home;
