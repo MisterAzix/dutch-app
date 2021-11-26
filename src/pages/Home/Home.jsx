09862601939f945156fd207d0edb08503490a738
MisterAzix <flizi@live.fr>
2021-11-26 17:56:19 +0100

    :sparkles: Player section & Global deck

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,39 +1,23 @@
 import React from "react";
-import logo from '../../logo.svg';
-import styles from './home.module.css';
+import styles from "./home.module.css";
 
-import Navbar from '../../components/NavBar/Navbar';
+import Navbar from "../../components/NavBar/Navbar";
 import PlayerDeck from "../../components/PlayerDeck/PlayerDeck";
+import Card from "../../components/Card/Card";
 
 const Home = () => {
     // let buttonName = 'Dutch!';
 
     return (
         <div className={styles.container}>
-
-            <PlayerDeck />
-            <Navbar />
-            
-            
-            
-            
-            
-            {/* <Button value={buttonName}></Button> */}
-
-            {/* <header className={styles.header}>
-                <img src={logo} className={styles.logo} alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className={styles.link}
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header> */}
+            <div className={styles.board}>
+                <Card symbol="club" value="8" />
+                <Card symbol="spade" value="4" />
+            </div>
+            <div className={styles.player_container}>
+                <PlayerDeck />
+                <Navbar />
+            </div>
         </div>
     );
 };
