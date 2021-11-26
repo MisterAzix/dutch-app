9f4312fb0b3a240a18d325af46f6be2c3ebddda9
SebastienBn <seb.aguilar@hotmail.com>
2021-11-26 15:53:19 +0100

    Card component back

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,16 +6,25 @@ import Heart from '../../icons/Heart';
 import Spade from '../../icons/Spade';
 import Club from '../../icons/Club';
 
-export default function Card(props) {
+export default function Card({ symbol, value, active }) {
 
     return (
-        <div className={`${styles.card} ${(props.symbol === "diamond"||props.symbol === "heart") ?styles.red:styles.black}`}>
-            <div className={styles.top}>{props.value}</div>
-                {props.symbol === "diamond" && <Diamond className={styles.icon} /> }
-                {props.symbol === "heart" && <Heart className={styles.icon} /> }
-                {props.symbol === "club" && <Club className={styles.icon} /> }
-                {props.symbol === "spade" && <Spade className={styles.icon} /> }
-            <div className={styles.bottom}>{props.value}</div>
+        <div className={`${active ? styles.card_active : styles.card} ${(symbol === "diamond" || symbol === "heart") ? styles.red : styles.black}`}>
+            {active ? 
+            <>
+                <div className={styles.top}>{value}</div>
+                    {symbol === "diamond" && <Diamond className={styles.icon} /> }
+                    {symbol === "heart" && <Heart className={styles.icon} /> }
+                    {symbol === "club" && <Club className={styles.icon} /> }
+                    {symbol === "spade" && <Spade className={styles.icon} /> }
+                <div className={styles.bottom}>{value}</div>
+            </> : 
+            <>
+                <div className={styles.top_back}></div>
+                <div className={styles.txt_back}>DUTCH<br />APP</div>
+                <div className={styles.bottom_back}></div>
+            </>}
+            
         </div>
     )
 }
